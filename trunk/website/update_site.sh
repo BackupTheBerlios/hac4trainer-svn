0d9e020e7e3ad19218248d14476854fb6d0df4d2
ibooij <ibooij@9386403a-7f03-0410-bd42-9d76b19637af>
2005-10-20 09:54:37 +0000

    moved website source to website/src updated update_site.sh
    deleted site directory

--- trunk/website/update_site.sh
+++ trunk/website/update_site.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+SITE_DIR=src
+REMOTE_LOCATION=ibooij@berlios:/home/groups/hac4trainer/htdocs
+
 # syncs remote site with local version
 # -z use compression
 # --delete delete files not present on sender's side
@@ -7,4 +10,4 @@
 # -v verbose
 # -e ssh use ssh as shell
 # -r recursive directories
-rsync -z --delete -u -v -e ssh -r site/* ibooij@berlios:/home/groups/hac4trainer/htdocs
+rsync -z --delete -u -v -e ssh -r $SITE_DIR/* $REMOTE_LOCATION
